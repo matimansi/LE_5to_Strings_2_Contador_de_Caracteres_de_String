c684a93468f30d4c9d8ced33d774ebfab0246b71
Matias Mansilla <mmansilla@etrr.edu.ar>
2022-11-18 19:44:31 -0300

    mejora a la legibilidad

--- Cuenta_Caracteres.c
+++ Cuenta_Caracteres.c
@@ -7,31 +7,29 @@ void Cuenta_Caracteres (char palabra[LIM]);
 
 int main()
 {
-	char palabra[LIM] = {'\0'};
-	printf("\nBienvenido a nuestro programa...\n");
-	printf("Ingrese una palabra: ");
-	fgets(palabra, LIM_FGETS, stdin);
-	for (int i = 0; i < LIM; i++)
-	{
-		if (palabra[i] == 10)		// En la tabla ASCII el 10 corresponde al ENTER
-		{
-			palabra[i] = '\0';		// Reemplazo el ENTER por un NULL para que la lectura de caracteres termine hasta esa posicion,
-		}							// ademas de la estetica, para que no imprima un ENTER en el ultimo printf()
-	}
-	Cuenta_Caracteres (palabra);
-	return 0;
+    char palabra[LIM] = {'\0'};
+    printf("\nBienvenido a nuestro programa...\n");
+    printf("Ingrese una palabra: ");
+    fgets(palabra, LIM_FGETS, stdin);
+    for (int i = 0; i < LIM; i++)
+    {
+        if (palabra[i] == 10) palabra[i] = '\0';
+    }
+/*
+	En la tabla ASCII el 10 corresponde al ENTER
+    Reemplazo el ENTER por un NULL para que la lectura de caracteres termine hasta esa posicion,
+    ademas de la estetica, para que no imprima un ENTER en el ultimo printf()
+*/
+    Cuenta_Caracteres (palabra);
+    return 0;
 }
 
 void Cuenta_Caracteres (char palabra [LIM])
 {
-	int i;
-	for (i = 0; i < LIM; i++)
-	{
-		if (palabra[i] == '\0')
-		{
-			break;
-		}
-	}
-	printf("El string ingresado (%s) tiene %d caracteres\n\n", palabra, i);
-	
+    int i;
+    for (i = 0; i < LIM; i++)
+    {
+        if (palabra[i] == '\0') break;
+    }
+    printf("El string ingresado (%s) tiene %d caracteres\n\n", palabra, i);
 }
